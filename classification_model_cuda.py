673b24b1d6ca818eb08c2cbf0a49f5338f61fad0
aishwarya-suyamindra <aishwaryasr012@gmail.com>
2024-04-05 23:51:40 -0400

    Updated embedding, hidden size dimensions

--- classification_model_cuda.py
+++ classification_model_cuda.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 from transformers import GPT2Tokenizer
-from transformers import GPT2Model
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,11 +15,7 @@ print(f"Using {device} for training")
 # Load the IMDB dataset
 dataset = load_dataset('imdb', split='train')
 
-tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
-
-with torch.no_grad():
-    attack_model = GPT2Model.from_pretrained('openai-community/gpt2', output_hidden_states = True).to(device)
-
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 
 class IMDbDataset(Dataset):
@@ -58,38 +53,37 @@ class PositionalEncoding(nn.Module):
         return x
 
 class TransformerDecoder(nn.Module):
-
-    def __init__(self, vocab_size, embed_dim=768, num_heads=2, hidden_dim=768, num_layers=1, dropout=0.1):
+    def __init__(self, vocab_size, embed_dim=768, num_heads=2, hidden_dim=2048, num_layers=1, dropout=0.1):
         super(TransformerDecoder, self).__init__()
-        # self.embedding = nn.Embedding(vocab_size, embed_dim)
-        # self.pos_encoder = PositionalEncoding(embed_dim)
-        self.embedding = attack_model.get_input_embeddings()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.pos_encoder = PositionalEncoding(embed_dim)
         decoder_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=num_heads, dim_feedforward=hidden_dim, dropout=dropout)
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
         self.fc = nn.Linear(embed_dim, 1)
         self.init_weights()
 
     def init_weights(self):
-        initrange = 0.1
-        # self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.fc.weight.data.uniform_(-initrange, initrange)
-        self.fc.bias.data.zero_()
+      initrange = 0.1
+      self.embedding.weight.data.uniform_(-initrange, initrange)
+      self.fc.weight.data.uniform_(-initrange, initrange)
+      self.fc.bias.data.zero_()
 
-    def forward(self, input_ids):
-        # embedded = self.embedding(input_ids)
-        # encoded = self.pos_encoder(embedded)
-        embedded = None
-        output = self.transformer_decoder(embedded, embedded)
-        output = output.mean(dim=1)
-        output = self.fc(output)
-        return torch.sigmoid(output).squeeze()
+    def forward(self, input_ids, input_embeds = None):
+      if input_embeds is None:
+        input_embeds = self.embedding(input_ids)
+      encoded = input_embeds
+      encoded = self.pos_encoder(encoded)
+      output = self.transformer_decoder(encoded, encoded)
+      output = output.mean(dim=1)
+      output = self.fc(output)
+      return torch.sigmoid(output).squeeze()
 
 model = TransformerDecoder(tokenizer.vocab_size).to(device)
 
-# model.load_state_dict(torch.load(r"model_states\model_epoch_20.pt"), strict=False)
+model.load_state_dict(torch.load(r"model_states\model_epoch_20.pt"), strict=False)
 
-# for param in model.parameters():
-#     param.requires_grad = True
+for param in model.parameters():
+    param.requires_grad = True
 
 def train(model, data_loader, epochs=20):
     
@@ -105,20 +99,20 @@ def train(model, data_loader, epochs=20):
         total_loss = 0
         for input_ids, labels in data_loader:
             input_ids, labels = input_ids.to(device), labels.to(device)
-            print(input_ids)
-            break
             optimizer.zero_grad()
             outputs = model(input_ids)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
-        break
-        if (epoch+1) % 5 == 0:
-            torch.save(model.state_dict(), r"updated_states/model_epoch_{}.pt".format(epoch+1))
+        
+        if (epoch+1) % 2 == 0:
+            torch.save(model.state_dict(), r"model_states/model_continued_epoch_{}.pt".format(epoch+1))
             
         print(f"Epoch {epoch+1}: Loss = {total_loss / len(data_loader)}")
         lr_scheduler.step()
+
+        
 
 if __name__ == '__main__':
     train(model, data_loader)
