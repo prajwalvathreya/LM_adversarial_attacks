1214612bcb4eb5bc2731e21fac2e8e8155e37d36
prajwalathreya <athreya.p@northeastern.edu>
2024-04-08 16:44:20 -0400

    created inference pipeline for attack model

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import GPT2Tokenizer
-from transformers import GPT2Model
 from classification_inference import inference, init_model
+from attacker_model_inference import *
 import torch
 
 
@@ -19,8 +19,10 @@ app.add_middleware(
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 
 inf_model = init_model(tokenizer, device)
 inf_model.eval()
@@ -40,3 +42,15 @@ def run_inference(input_data: InputData):
     result = inference(inf_model, tokenizer, device, input_data.input_text)
 
     return {"result": result}
+
+
+@app.post("/adversarial_example")
+def gen_adversarial_example(input_data: InputData, input_label: int):
+
+    target_model = load_target_model(device)
+    attacker_model = load_attacker_model(device)
+    processed_dataset = process_dataset(tokenizer, input_data.input_text, device)
+    tm_embeddings, am_embeddings = load_embeddings(tokenizer, device, target_model, attacker_model)
+    original_pred, original_text, adversarial_text, adversarial_pred = train(target_model, attacker_model, inputs = processed_dataset['input_ids'], labels = torch.tensor(input_label, dtype=torch.long), am_embeddings = am_embeddings.to(device), tm_embeddings = tm_embeddings.to(device), device = device, tokenizer = tokenizer)
+    
+    return {"original_text": original_text, "original_pred": original_pred, "adversarial_text": adversarial_text, "adversarial_pred": adversarial_pred}
